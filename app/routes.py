5bc87e2b3071234894ad22e2a69e7024e677ae2b
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-08 18:39:42 -0600

    connected hyml

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
 @bp.route('/')
 def main():
-    return "calender working"
-    
+    return render_template('main.html')
