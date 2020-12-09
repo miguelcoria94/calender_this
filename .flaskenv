4a285d9788f2e4e078d59608bec97fbf412a4509
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-08 18:34:56 -0600

    creat env variables

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=calender_this.py
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,8 @@
 from flask import Blueprint
 
+bp = Blueprint("main", __name__, url_prefix="/")
+
+@bp.route('/')
+def main():
+    return "calender working"
+    
