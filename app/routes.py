96cebdb0b060b9fba9c8c54cf15a1e0d18848347
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-08 19:03:13 -0600

    render db stuff

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
+import os
+import psycopg2
 from flask import Blueprint, render_template
 
 bp = Blueprint("main", __name__, url_prefix="/")
 
+CONNECTION_PARAMETERS = {
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASS"),
+    "dbname": os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+}
+
 @bp.route('/')
 def main():
-    return render_template('main.html')
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            curs.execute("select id, name, start_staetime, end_datetime, description, private from appointments")
+            result = curs.fetchall()
+        return render_template('main.html', result = result)
